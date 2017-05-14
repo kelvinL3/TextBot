f749b3894989d4bd4cbffa683b620015722de4d5
Aaron Kau <aaron.kau@rutgers.edu>
2017-05-14 04:52:38 -0400

    Test

--- ChromeExtension/selector.js
+++ ChromeExtension/selector.js
@@ -1,21 +1,23 @@
 var imgs = document.getElementsByTagName("img");
-var olds = [];
-var is = [];
+var olds;
+var is;
 
 for (var i=0;i<imgs.length;i++) {
 	var rand = Math.random();
 	var val = parseInt(Math.random() * 66);
 	if (rand < 0.05) {
-		olds.push(imgs[i].src);
-		is.push(i);
+		olds = imgs[i].src;
+		is = i;
 		imgs[i].src = "http://www.aaronkau.com/riyinator8000/Riyas/riya"+val+".jpg";
+		break;
 	}
 
-	setTimeout(function() { returnImages(); }, 5000);
+
+	
 }
 
+setTimeout(function() { returnImages(); }, 5000);
+
 function returnImages() {
-	for (var i=0;i<is.length;i++) {
-		imgs[is[i]].src = olds[i];
-	}
+	imgs[is].src = olds;
 }
